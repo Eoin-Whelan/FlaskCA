d938e136397fee58bc477fadfc3688aa61022ba6
Eoin Farrell C00164354 <c00164354@itcarlow.ie>
2020-10-31 17:02:12 +0000

    Second commit. Set up the guestbook functionality to save user information. Also fleshed out currently working template for Jinja2.

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 """
-Title:		webApp.py
+Title:		app.py
 Author:	Eoin Farrell
 Student No:	C00164354
-Purpose:	webApp.py provides the Flask Controller for the website.
 DOC:		30/10/2020
 """
 
@@ -11,10 +10,42 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+"""
+For display_homepage, it has an additional ovveride of the basic function
+so that when the user enters their information their name is displayed back to
+them with an alternative greeting.
+"""
 
 @app.route("/")
 def display_homepage():
-    return render_template("index.html", main_title="The Eoin Zone")
+    return render_template(
+        "index.html",
+        main_title="The Eoin Zone",
+        userName="Visitor",
+        open_message="If you would be so kind as to sign the guestbook before continuing, I would be so grateful",
+    )
+
+
+def display_homepage(guest_Signed):
+    return render_template(
+        "index.html",
+        main_title="The Eoin Zone",
+        userName=guest_Signed,
+        open_message="Thanks! Feel free to check out this website through the menu above!",
+    )
+
+
+@app.route("/processForm", methods=["POST"])
+def process_form_data():
+    data = request.form
+
+    with open("comments.txt", "a") as df:
+        print(data["userName"], ",", sep="", end="", file=df)
+        print(data["userEmail"], ",", sep="", end="", file=df)
+        print(data["userMsg"], ",", sep="", end="", file=df)
+        df.write("\n")
+
+    return display_homepage(data["userName"])
 
 
 @app.route("/personalPage")
@@ -29,7 +60,9 @@ def cv():
 
 @app.route("/techInterests")
 def techInterests():
-    return render_template("techinterests.html", main_title="What Draws Me To Soft. Engineering")
+    return render_template(
+        "techinterests.html", main_title="What Draws Me To Soft. Engineering"
+    )
 
 
 @app.route("/assistiveTech")
